3e54a25bb8b3fa603aabdd5bb91d3aa22b21fd19
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-02-02 19:28:39 +0000

    Updated the js code/ api key and debugged the html search input

--- Assets/JS/scriptrecommendcal.js
+++ Assets/JS/scriptrecommendcal.js
@@ -1,32 +1,38 @@
-$(document).ready(function() { //Placeholder code for the recommend app. 
-
+$(document).ready(function() {
     $('#search-button').on('click', function() {
-
+        var age = $('#age').val();
+        var height = $('#height').val();
+        var weight = $('#weight').val();
+        var gender = $('#gender').val();
+        
         var foodItem = $('#search-input').val().trim();
 
-
-        if (foodItem) {
-
+        if (age && height && weight && gender && foodItem) {
             recommendCal(age, height, weight, gender);
         } else {
-
-            alert('Please enter your data!');
+            alert('Please fill in all the fields!');
         }
     });
 });
 
 function recommendCal(age, height, weight, gender) {
-    //var queryURL = "https://calorie-calculator.p.rapidapi.com/caloriecalculator.php/apiKey=" + apiKey
-
-    var queryURL = "https://calorie-calculator.p.rapidapi.com/caloriecalculator.php?age=18&height=177&weight=112&gender=male&apiKey=" + apiKey;
+    // Use woking API key
+    var apiKey = '922f974a17mshf6800fada2de78ap1ead14jsn3d1a5fbddb77';
+    var queryURL = "https://calorie-calculator.p.rapidapi.com/caloriecalculator.php?age=" + age + "&height=" + height + "&weight=" + weight + "&gender=" + gender + "&apiKey=" + apiKey;
 
     fetch(queryURL)
         .then(function(response) {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
             return response.json();
         })
         .then(function(data) {
             console.log(queryURL);
             console.log(data);
-
+            // Process the API response data here
         })
+        .catch(function(error) {
+            console.error('Error during fetch operation:', error);
+        });
 }
