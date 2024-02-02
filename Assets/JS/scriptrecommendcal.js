7767d64b5ace96369bf8a4340a60b4403874347b
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-02-02 19:34:31 +0000

    used code snippet provided by rapidapi

--- Assets/JS/scriptrecommendcal.js
+++ Assets/JS/scriptrecommendcal.js
@@ -15,24 +15,39 @@ $(document).ready(function() {
     });
 });
 
-function recommendCal(age, height, weight, gender) {
-    // Use woking API key
+async function recommendCal(age, height, weight, gender) {
+    // Use working API key
     var apiKey = '922f974a17mshf6800fada2de78ap1ead14jsn3d1a5fbddb77';
     var queryURL = "https://calorie-calculator.p.rapidapi.com/caloriecalculator.php?age=" + age + "&height=" + height + "&weight=" + weight + "&gender=" + gender + "&apiKey=" + apiKey;
 
-    fetch(queryURL)
-        .then(function(response) {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(function(data) {
-            console.log(queryURL);
-            console.log(data);
-            // Process the API response data here
-        })
-        .catch(function(error) {
-            console.error('Error during fetch operation:', error);
-        });
+    try {
+        const response = await fetch(queryURL);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        console.log(queryURL);
+        console.log(data);
+        // Process the API response data here
+    } catch (error) {
+        console.error('Error during fetch operation:', error);
+    }
+}
+
+// The code below can be used for a separate API request but isn't connected to the button click event.
+const url = 'https://calories-daily-calculator.p.rapidapi.com/calories/?age=30&weight=80&height=182';
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '922f974a17mshf6800fada2de78ap1ead14jsn3d1a5fbddb77',
+        'X-RapidAPI-Host': 'calories-daily-calculator.p.rapidapi.com'
+    }
+};
+
+try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    console.log(result);
+} catch (error) {
+    console.error(error);
 }
