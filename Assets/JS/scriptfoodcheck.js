c51db926e53418f1224d8b148f56db0ea17b8463
Antimio <antimiocasimiro@gmail.com>
2024-02-02 18:02:21 +0000

    Created the page-on-load listener that will trigger the app. Also created fetch blocks for both working pages - Fetch not working as the api is being denied access

---
+++ Assets/JS/scriptfoodcheck.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+    $('#search-button').on('click', function() {
+
+        var foodItem = $('#search-input').val().trim();
+
+
+        if (foodItem) {
+
+            fetchCalorieData(foodItem);
+        } else {
+
+            alert('Please enter a food item!');
+        }
+    });
+});
+
+function fetchCalorieData(foodItem) {
+    //var queryURL = "https://calorie-calculator.p.rapidapi.com/caloriecalculator.php/apiKey=" + apiKey
+
+    var queryURL = "https://calorie-calculator.p.rapidapi.com/caloriecalculator.php?age=18&height=177&weight=112&gender=male&apiKey=" + apiKey;
+
+    fetch(queryURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            console.log(queryURL);
+            console.log(data);
+
+        })
+}
