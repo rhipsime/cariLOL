536c30ae2c446a98431ed3b0092f0a22e8841060
rhipsime <151936395+rhipsime@users.noreply.github.com>
2024-02-06 19:56:40 +0000

    removed unnecesary code

--- Assets/JS/apikey.js
+++ Assets/JS/apikey.js
@@ -3,4 +3,4 @@ rapidApi:
 var apiKey = "922f974a17mshf6800fada2de78ap1ead14jsn3d1a5fbddb77"
 edamameApi:
 var apiKey = "d1e1d9b3"// apikey.js
-var apiKey = "922f974a17mshf6800fada2de78ap1ead14jsn3d1a5fbddb77"
+
