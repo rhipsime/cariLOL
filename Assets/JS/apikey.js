4fe6ad792acc1a11a06ef0f386e5f9ce765eb7ba
Antimio <antimiocasimiro@gmail.com>
2024-02-07 20:16:45 +0000

    added code to render random recipes to the user, according to the ingredients entered in the search

--- Assets/JS/apikey.js
+++ Assets/JS/apikey.js
@@ -3,4 +3,6 @@ rapidApi:
     var apiKey = "922f974a17mshf6800fada2de78ap1ead14jsn3d1a5fbddb77"
 edamameApi:
     //var apiKey = "d1e1d9b3" // apikey.js
-    var apiKey1 = "20d3e48b1ae1da2c0cd8d248cad697776b"
+    var apiKeyNutri = "20d3e48b1ae1da2c0cd8d248cad697776b"
+
+var apiKeyRecipe = "f9d939aedacda382e27063089e081fb2"
