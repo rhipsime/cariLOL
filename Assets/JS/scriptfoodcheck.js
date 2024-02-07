a4bfdefe075d0b0fd441ab1f01cc4474c3093c0e
kamilawroblewska <kamila.wroblewska15@gmail.com>
2024-02-07 22:29:10 +0000

    Sorted out underline line in recipe links; added background and font color

--- Assets/JS/scriptfoodcheck.js
+++ Assets/JS/scriptfoodcheck.js
@@ -1,145 +1,200 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $("#search-section").on("submit", function (event) {
+    event.preventDefault();
 
-    $("#search-section").on("submit", function(event) {
-        event.preventDefault();
+    var foodItem = $("#search-input-food").val().trim();
 
-        var foodItem = $("#search-input-food").val().trim();
+    var amount = $("#search-input-amount").val().trim();
 
-        var amount = $("#search-input-amount").val().trim();
+    var unitWeight = $("#search-input-units").val().trim();
 
-        var unitWeight = $("#search-input-units").val().trim();
-
-        if (foodItem) {
-
-            fetchCalorieData(foodItem, amount, unitWeight);
-
-        } else {
-
-
-            showModal("Please enter at least one food item. If no weight is provided, it will give the result per 100 grams!");
-
-        }
-    });
+    if (foodItem) {
+      fetchCalorieData(foodItem, amount, unitWeight);
+    } else {
+      showModal(
+        "Please enter at least one food item. If no weight is provided, it will give the result per 100 grams!"
+      );
+    }
+  });
 });
 
 function showModal(message) {
-    $("#infoModal .modal-body").text(message);
-    $("#infoModal").modal("show");
+  $("#infoModal .modal-body").text(message);
+  $("#infoModal").modal("show");
 }
 
 function fetchCalorieData(foodItem, amount, unitWeight) {
+  var queryURL1 =
+    "https://api.edamam.com/api/nutrition-data?app_id=adbad49e&app_key=%" +
+    apiKeyNutri +
+    "&nutrition-type=cooking&ingr=" +
+    amount +
+    "%20" +
+    unitWeight +
+    "%20" +
+    foodItem;
 
-    var queryURL1 = "https://api.edamam.com/api/nutrition-data?app_id=adbad49e&app_key=%" + apiKeyNutri + "&nutrition-type=cooking&ingr=" + amount + "%20" + unitWeight + "%20" + foodItem;
+  var queryURL2 =
+    "https://api.edamam.com/api/nutrition-data?app_id=adbad49e&app_key=%" +
+    apiKeyNutri +
+    "&nutrition-type=cooking&ingr=100%20g%20" +
+    foodItem;
 
-    var queryURL2 = "https://api.edamam.com/api/nutrition-data?app_id=adbad49e&app_key=%" + apiKeyNutri + "&nutrition-type=cooking&ingr=100%20g%20" + foodItem;
+  var queryURL3 =
+    "https://api.edamam.com/api/recipes/v2?type=public&q=" +
+    foodItem +
+    "&app_id=f783db65&app_key=" +
+    apiKeyRecipe +
+    "&random=true";
 
-    var queryURL3 = "https://api.edamam.com/api/recipes/v2?type=public&q=" + foodItem + "&app_id=f783db65&app_key=" + apiKeyRecipe + "&random=true";
+  if (foodItem && amount && unitWeight) {
+    fetch(queryURL1)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (dataNutri) {
+        console.log(queryURL1);
+        console.log(dataNutri);
 
-    if (foodItem && amount && unitWeight) {
+        nutritionalInfo(dataNutri);
+      });
+  } else {
+    fetch(queryURL2)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (dataNutri) {
+        console.log(queryURL2);
+        console.log(dataNutri);
 
-        fetch(queryURL1)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(dataNutri) {
-                console.log(queryURL1);
-                console.log(dataNutri);
+        nutritionalInfo(dataNutri);
+      });
+  }
 
-                nutritionalInfo(dataNutri);
+  fetch(queryURL3)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (dataRecipe) {
+      console.log(queryURL3);
+      console.log(dataRecipe);
 
-            })
+      console.log(dataRecipe.hits[1].recipe.label);
 
-    } else {
+      recipe(dataRecipe);
+    });
 
-        fetch(queryURL2)
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(dataNutri) {
-                console.log(queryURL2);
-                console.log(dataNutri);
+  var results = $("#results-section").addClass(
+    "d-block row align-items-center"
+  );
 
-                nutritionalInfo(dataNutri);
+  function nutritionalInfo(dataNutri) {
+    results.empty();
 
-            })
-    }
+    var nutritionSection = $("<div>")
+      .addClass(
+        "d-block align-items-center justify-content-center container-fluid nutrition-div"
+      )
+      .empty();
 
-    fetch(queryURL3)
-        .then(function(response) {
-            return response.json();
-        })
-        .then(function(dataRecipe) {
-            console.log(queryURL3);
-            console.log(dataRecipe);
+    var units = unitWeight || "g";
 
-            console.log(dataRecipe.hits[1].recipe.label);
+    var titleNutri = $("<h2>")
+      .addClass("card-title nutri text-black")
+      .text(
+        "Nutritional data for " +
+          dataNutri.totalWeight +
+          units +
+          " of " +
+          foodItem
+      );
 
-            recipe(dataRecipe);
+    var calories = $("<p>")
+      .addClass("card-body nutriDes text-black")
+      .text("Calories: " + dataNutri.calories + " kcal");
 
-        })
+    var protein = $("<p>")
+      .addClass("card-body nutriDes text-black")
+      .text("Protein: " + dataNutri.totalNutrients.FAT.quantity + " g");
 
-    var results = $("#results-section").addClass("d-block row align-items-center");
+    var fat = $("<p>")
+      .addClass("card-body nutriDes text-black")
+      .text("Fat: " + dataNutri.totalNutrients.FAT.quantity + " g");
 
+    var carbs = $("<p>")
+      .addClass("card-body nutriDes text-black")
+      .text(
+        "Carbohydrates : " + dataNutri.totalNutrients.CHOCDF.quantity + " g"
+      );
 
-    function nutritionalInfo(dataNutri) {
+    nutritionSection.append(titleNutri.append(calories, protein, fat, carbs));
 
-        results.empty();
+    results.append(nutritionSection);
+  }
 
-        var nutritionSection = $("<div>").addClass("d-block align-items-center justify-content-center container-fluid nutrition-div").empty();
+  function recipe(dataRecipe) {
+    var recipeSection = $("<div>")
+      .addClass(
+        "card col-md-9 d-flex flex-row align-items-center justify-content-center container-fluid recContainer"
+      )
+      .empty();
 
-        var units = unitWeight || "g";
+    recipeSection.css("height", "auto");
 
-        var titleNutri = $("<h2>").addClass("card-title nutri text-black").text("Nutritional data for " + dataNutri.totalWeight + units + " of " + foodItem);
+    var recipe1 = $("<div>")
+      .addClass("col-md-4 bg-info p-1 recipe-div")
+      .css(
+        "background-image",
+        "url('" + dataRecipe.hits[0].recipe.image + "')"
+      );
 
-        var calories = $("<p>").addClass("card-body nutriDes text-black").text("Calories: " + dataNutri.calories + " kcal");
+    var titleRecipe1 = $("<a>", {
+      href: dataRecipe.hits[0].recipe.url,
+      target: "_blank",
+      html:
+        "<h4 class='card-title linkRes btn'>" +
+        dataRecipe.hits[0].recipe.label +
+        "</h4>",
+    });
 
-        var protein = $("<p>").addClass("card-body nutriDes text-black").text("Protein: " + dataNutri.totalNutrients.FAT.quantity + " g");
+    var recipe2 = $("<div>")
+      .addClass("col-md-4 bg-info p-1 recipe-div")
+      .css(
+        "background-image",
+        "url('" + dataRecipe.hits[10].recipe.image + "')"
+      );
 
-        var fat = $("<p>").addClass("card-body nutriDes text-black").text("Fat: " + dataNutri.totalNutrients.FAT.quantity + " g");
+    var titleRecipe2 = $("<a>", {
+      href: dataRecipe.hits[10].recipe.url,
+      target: "_blank",
+      html:
+        "<h4 class='card-title linkRes btn'>" +
+        dataRecipe.hits[10].recipe.label +
+        "</h4>",
+    });
 
-        var carbs = $("<p>").addClass("card-body nutriDes text-black").text("Carbohydrates : " + dataNutri.totalNutrients.CHOCDF.quantity + " g");
+    var recipe3 = $("<div>")
+      .addClass("col-md-4 bg-info p-1 recipe-div")
+      .css(
+        "background-image",
+        "url('" + dataRecipe.hits[19].recipe.image + "')"
+      );
 
-        nutritionSection.append(titleNutri.append(calories, protein, fat, carbs));
+    var titleRecipe3 = $("<a>", {
+      href: dataRecipe.hits[19].recipe.url,
+      target: "_blank",
+      html:
+        "<h4 class='card-title linkRes btn'>" +
+        dataRecipe.hits[19].recipe.label +
+        "</h4>",
+    });
 
-        results.append(nutritionSection);
+    recipeSection.append(
+      recipe1.append(titleRecipe1),
+      recipe2.append(titleRecipe2),
+      recipe3.append(titleRecipe3)
+    );
 
-    }
-
-    function recipe(dataRecipe) {
-
-        var recipeSection = $("<div>").addClass("card col-md-9 d-flex flex-row align-items-center justify-content-center container-fluid recContainer").empty();
-
-        recipeSection.css("height", "auto")
-
-        var recipe1 = $("<div>").addClass("col-md-4 bg-info p-1 recipe-div").css("background-image", "url('" + dataRecipe.hits[0].recipe.image + "')");
-
-        var titleRecipe1 = $("<a>", {
-            href: dataRecipe.hits[0].recipe.url,
-            target: "_blank",
-            html: "<h4 class='card-title text-decoration-none'>" + dataRecipe.hits[0].recipe.label + "</h4>"
-        });
-
-        var recipe2 = $("<div>").addClass("col-md-4 bg-info p-1 recipe-div").css("background-image", "url('" + dataRecipe.hits[10].recipe.image + "')");
-
-        var titleRecipe2 = $("<a>", {
-            href: dataRecipe.hits[10].recipe.url,
-            target: "_blank",
-            html: "<h4 class='card-title text-decoration-none'>" + dataRecipe.hits[10].recipe.label + "</h4>"
-        });
-
-        var recipe3 = $("<div>").addClass("col-md-4 bg-info p-1 recipe-div").css("background-image", "url('" + dataRecipe.hits[19].recipe.image + "')");;
-
-        var titleRecipe3 = $("<a>", {
-            href: dataRecipe.hits[19].recipe.url,
-            target: "_blank",
-            html: "<h4 class='card-title text-decoration-none'>" + dataRecipe.hits[19].recipe.label + "</h4>"
-        });
-
-        recipeSection.append(recipe1.append(titleRecipe1), recipe2.append(titleRecipe2), recipe3.append(titleRecipe3));
-
-        results.append(recipeSection);
-
-    }
-
-
+    results.append(recipeSection);
+  }
 }
--- Assets/JS/scriptrecommendcal.js
+++ Assets/JS/scriptrecommendcal.js
@@ -1,77 +1,83 @@
-$(document).ready(function() {
-    $('#search-button').on('click', function() {
-        var height = $('#height').val();
-        var weight = $('#weight').val();
-        
-        if (height && weight) {
-            recommendBMI(weight, height); // Pass weight first, then height
-        } else {
-            alert('Please fill in both height and weight fields.');
-        }
-    });
+$(document).ready(function () {
+  $("#search-button").on("click", function () {
+    var height = $("#height").val();
+    var weight = $("#weight").val();
+
+    if (height && weight) {
+      recommendBMI(weight, height); // Pass weight first, then height
+    } else {
+      alert("Please fill in both height and weight fields.");
+    }
+  });
 });
 
-async function recommendBMI(weight, height) { // Change the order of parameters
-    var apiKey = '760816e53fmsh77a4795b4cad944p131f2djsn992310e8cb24';
-    var url = 'https://body-mass-index-bmi-calculator.p.rapidapi.com/metric?weight=' + weight + '&height=' + height; // Change the order of parameters
-    var options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': apiKey,
-            'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
-        }
-    };
+async function recommendBMI(weight, height) {
+  // Change the order of parameters
+  var apiKey = "760816e53fmsh77a4795b4cad944p131f2djsn992310e8cb24";
+  var url =
+    "https://body-mass-index-bmi-calculator.p.rapidapi.com/metric?weight=" +
+    weight +
+    "&height=" +
+    height; // Change the order of parameters
+  var options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": apiKey,
+      "X-RapidAPI-Host": "body-mass-index-bmi-calculator.p.rapidapi.com",
+    },
+  };
 
-    try {
-        const response = await fetch(url, options);
-        const data = await response.json();
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
 
-        // Display the fetched data
-        displayBMIRecommendation(data);
-    } catch (error) {
-        console.error('Error during fetch operation:', error);
-    }
+    // Display the fetched data
+    displayBMIRecommendation(data);
+  } catch (error) {
+    console.error("Error during fetch operation:", error);
+  }
 }
 
 function displayBMIRecommendation(data) {
-    // Get the result section element
-    const resultSection = document.getElementById('result-output');
+  // Get the result section element
+  const resultSection = document.getElementById("result-output");
 
-    // Clear any previous content
-    resultSection.innerHTML = '';
+  // Clear any previous content
+  resultSection.innerHTML = "";
 
-    // Create elements to display the BMI recommendation data
-    const bmiTitle = document.createElement('h2');
-    bmiTitle.textContent = 'BMI Recommendation:';
+  // Create elements to display the BMI recommendation data
+  const bmiTitle = document.createElement("h2");
+  bmiTitle.textContent = "BMI Recommendation:";
 
-    const bmiValue = document.createElement('p');
-    bmiValue.textContent = 'BMI: ' + data.bmi.toFixed(1); // Round to 1 decimal point
+  const bmiValue = document.createElement("p");
+  bmiValue.textContent = "BMI: " + data.bmi.toFixed(1); // Round to 1 decimal point
 
-    const heightValue = document.createElement('p');
-    heightValue.textContent = 'Height: ' + data.height + ' cm';
+  const heightValue = document.createElement("p");
+  heightValue.textContent = "Height: " + data.height + " cm";
 
-    const weightValue = document.createElement('p');
-    weightValue.textContent = 'Weight: ' + data.weight + ' kg';
+  const weightValue = document.createElement("p");
+  weightValue.textContent = "Weight: " + data.weight + " kg";
 
-    const weightCategory = document.createElement('p');
-    weightCategory.textContent = 'Weight Category: ' + determineWeightCategory(data.bmi);
+  const weightCategory = document.createElement("p");
+  weightCategory.textContent =
+    "Weight Category: " + determineWeightCategory(data.bmi);
 
-    // Append the elements to the result section
-    resultSection.appendChild(bmiTitle);
-    resultSection.appendChild(bmiValue);
-    resultSection.appendChild(heightValue);
-    resultSection.appendChild(weightValue);
-    resultSection.appendChild(weightCategory);
+  // Append the elements to the result section
+  resultSection.appendChild(bmiTitle);
+  resultSection.appendChild(bmiValue);
+  resultSection.appendChild(heightValue);
+  resultSection.appendChild(weightValue);
+  resultSection.appendChild(weightCategory);
 }
 
 function determineWeightCategory(bmi) {
-    if (bmi < 18.5) {
-        return 'Underweight';
-    } else if (bmi >= 18.5 && bmi < 25) {
-        return 'Normal weight';
-    } else if (bmi >= 25 && bmi < 30) {
-        return 'Overweight';
-    } else {
-        return 'Obese';
-    }
+  if (bmi < 18.5) {
+    return "Underweight";
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return "Normal weight";
+  } else if (bmi >= 25 && bmi < 30) {
+    return "Overweight";
+  } else {
+    return "Obese";
+  }
 }
